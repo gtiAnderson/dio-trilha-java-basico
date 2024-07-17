b7ca719409ed14fdc229dc34ab0a6528f7bf5351
Anderson Moreira <andersonmoreira507@gmail.com>
2024-07-17 12:44:58 -0300

    commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //TODO importar e conhecer a classe scanner
+        //Exibir as mensagens para o novo usuario
+        //Obter pelo scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+    
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = 25 ;
+        double valorSolicitado = 22 ;
+
+        if (saldo >= valorSolicitado){
+            saldo -= valorSolicitado;
+            System.out.println("Novo saldo = " + saldo + ("R$"));
+        }else  {
+            System.out.println("Saldo Insuficiente  ");
+        }
+        System.out.println( "Saldo atual = " + saldo + ("R$"));
+        scanner.close();
     }
 }
